0750ed24abb677d4c6a1562007b3f42d145141ed
Daniil Vysotskiy <daniil.vysotskiy@teligent.ru>
2017-12-21 12:15:39 +0300

    homework_02: task #1: simplyfied version

--- homework_02/timer.js
+++ homework_02/timer.js
@@ -1,92 +1,24 @@
 const http = require('http');
-const pug = require('pug');
-const path = require('path');
+const util = require('util');
+const setTimeoutPromise = util.promisify(setTimeout);
 
-const index = path.join(__dirname, '/views/index.pug');
+const INTERVAL = process.env.TIME_INTERVAL || 1000;
+const TIMEOUT = process.env.TIMEOUT || 5000;
+const PORT = process.env.PORT || 3000;
 
-let timerInterval = process.argv[2] || null;
-let timerLast = process.argv[3] || null;
-// e.g. node timer 1000 10000 -- show time every sec for 10 secs
+http.createServer(function (request, response) {
+  let linkInterval = setInterval(() => {
+    const currentTime = new Date().toUTCString();
 
-const stdin = process.stdin;
-const stdout = process.stdout;
+    console.log(currentTime);
+    response.write(currentTime + '\n');
+  }, INTERVAL);
 
-function ask (question, validate) {
-  return new Promise((resolve, reject) => {
-    stdin.resume();
-    stdout.write(`${question}: `);
-
-    stdin.once('data', data => {
-      data = data.toString().trim();
-
-      if (validate.test(data)) {
-        resolve(data);
-      } else {
-        stdout.write(`It should match: ${validate}\n`);
-        resolve(ask(question, validate));
-      }
-    });
+  setTimeoutPromise(TIMEOUT).then(() => {
+    console.log('DONE');
+    clearInterval(linkInterval);
+    response.end();
   });
-}
-
-async function askProcess () {
-  try {
-    timerInterval = await ask('Write timer interval in miliseconds', /\d*/);
-    timerLast = await ask('Write timer lasts in miliseconds', /\d*/);
-    startServer();
-    console.log('Http server started. Send http request via browser to see the timer.');
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-async function initProcess (interval, last) {
-  try {
-    await timer(interval, last);
-    startServer();
-    console.log('Http server started. Send http request via browser to see the timer.');
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-function timer (interval, last, setTimeoutCb, finalCb) {
-  const startTime = new Date().getTime();
-  const period = setInterval(() => {
-    if (new Date().getTime() - startTime > last) {
-      if (finalCb && typeof finalCb === 'function') {
-        finalCb();
-      }
-      clearInterval(period);
-    }
-
-    let currentTimeGMT = new Date().toISOString();
-    if (setTimeoutCb && typeof setTimeoutCb === 'function') {
-      setTimeoutCb(currentTimeGMT);
-    }
-  }, interval);
-}
-
-function startServer () {
-  http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    const compiledFunction = pug.compileFile(index);
-
-    if (request.headers.accept.match('text/html')) {
-      timer(timerInterval, timerLast, str => {
-        console.log('Current time (GMT): ' + str);
-        response.write(compiledFunction({f: str}));
-      }, () => {
-        setTimeout(() => {
-          response.end();
-        }, 0);
-      });
-    }
-  }).listen(3000);
-}
-
-if (!timerInterval && !timerLast) {
-  askProcess();
-} else {
-  initProcess(timerInterval, timerLast);
-}
+}).listen(PORT).on('listening', function() {
+  console.log('HTTP listening:' + PORT);
+});
