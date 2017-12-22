0479bdf19a562bc9e32e836569f8c316f4b701ef
Daniil Vysotskiy <daniil.vysotskiy@teligent.ru>
2017-12-22 16:17:33 +0300

    login handler

--- homework_03/app.js
+++ homework_03/app.js
@@ -32,7 +32,7 @@ app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
--- homework_03/routes/login.js
+++ homework_03/routes/login.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const db = require('../models/db');
 
 /* GET login page. */
 router.get('/', (req, res, next) => {
   res.render('pages/login', { title: 'Авторизация' });
+});
+
+router.post('/', (req, res, next) => {
+  const login = req.body.login;
+  const password = req.body.password;
+  
+  const dbUser = db.get('users').find({ login: login, password: password }).value();
+
+  if(!dbUser) {
+    const err = new Error();
+    err.status = 'Error';
+    err.mes = 'Логин и/или пароль введены неверно!';
+    res.send(err);
+  } else {
+    const response = {};
+    response.status = 'OK';
+    response.mes = 'Aвторизация успешна!';
+    res.send(response);
+  }
+
 });
 
 module.exports = router;
